3c2d52073358fcb8a0da5fd0a219497a163272aa
Onwuzor Victor <onwuzorvictor@outlook.com>
2020-12-23 18:21:39 +0100

    create footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer = () => {
+
+    return (
+        <footer className="footer flex-margin-top-auto">
+            <div className="content has-text-centered">
+                <p>
+                    Mesh Copyright &copy; 2020. 
+                    The source code is licensed <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. 
+                </p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
