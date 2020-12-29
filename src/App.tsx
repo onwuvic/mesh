5633cebb09bd69874f371f693a25130ade73a581
Onwuzor Victor <onwuzorvictor@outlook.com>
2020-12-29 11:05:25 +0100

    login functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,32 @@
 import React from 'react';
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import './assets/scss/main.scss';
+import PrivateRoute from './PrivateRoute';
+import UserProvider from './providers/UserProvider';
 import Home from './views/Home/Home';
 import LoginPage from './views/LoginPage/LoginPage';
 import SignupPage from './views/SignupPage/SignupPage';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path='/'>
-          <Redirect to="/orders" />
-        </Route>
-        <Route path='/login'>
-          <LoginPage />
-        </Route>
-        <Route path='/signup'>
-          <SignupPage />
-        </Route>
-        <Route path='/orders'>
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+    <UserProvider>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Redirect to="/orders" />
+          </Route>
+          <Route path='/login'>
+            <LoginPage />
+          </Route>
+          <Route path='/signup'>
+            <SignupPage />
+          </Route>
+          <PrivateRoute path='/orders'>
+            <Home />
+          </PrivateRoute>
+        </Switch>
+      </Router>
+    </UserProvider>
   );
 }
 
