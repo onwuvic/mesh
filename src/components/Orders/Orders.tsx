f98135487fe75fb7915090f45eda129bc61aa4af
Onwuzor Victor <onwuzorvictor@outlook.com>
2021-01-02 15:46:47 +0100

    get all orders and display it

--- src/components/Orders/Orders.tsx
+++ src/components/Orders/Orders.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Firebase from '../../helpers/Firebase';
 
 const Orders = () => {
+    const [orders, setOrders] = useState([]);
 
-    return (
-        <div>
-            <span className="is-size-5">
-                Ordered Lists
-            </span>
+    useEffect(() => {
+        Firebase.firestore.collection("/orders")
+            .onSnapshot((snapshot: any) => {
+                const listItems = snapshot.docs.map((doc: any) => ({
+                    id: doc.id,
+                    ...doc.data(),
+                }))
+                setOrders(listItems);
+            });
+    }, []);
 
+    function renderOrders() {
+        if (orders.length === 0 ) {
+            return (<h1>No Orders Found</h1>);
+        }
+
+        return (
             <table className="table is-fullwidth is-hoverable is-striped u-margin-top">
                 <thead>
                     <tr>
@@ -18,32 +31,28 @@ const Orders = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Test Order 1</td>
-                        <td>22.06.2019</td>
-                        <td>Wriezener Str. 12</td>
-                        <td>Emad Alam</td>
-                    </tr>
-                    <tr>
-                        <td>Test Order 1</td>
-                        <td>22.06.2019</td>
-                        <td>Wriezener Str. 12</td>
-                        <td>Emad Alam</td>
-                    </tr>
-                    <tr>
-                        <td>Test Order 1</td>
-                        <td>22.06.2019</td>
-                        <td>Wriezener Str. 12</td>
-                        <td>Emad Alam</td>
-                    </tr>
-                    <tr>
-                        <td>Test Order 1</td>
-                        <td>22.06.2019</td>
-                        <td>Wriezener Str. 12</td>
-                        <td>Emad Alam</td>
-                    </tr>
+                     {
+                         orders.map((order: any) => (
+                            <tr key={order.id}>
+                                <td>{order.title}</td>
+                                <td>date</td>
+                                <td>street </td>
+                                <td>name</td>
+                            </tr>
+                        ))
+                     }
                 </tbody>
-            </table>            
+            </table>
+        )
+    }
+
+    return (
+        <div>
+            <span className="is-size-5">
+                Ordered Lists
+            </span>
+
+            { renderOrders() }        
         </div>            
     )
 }
