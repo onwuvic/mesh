045f68597f1741685d4a1908a806a797beaaa3af
Onwuzor Victor <onwuzorvictor@outlook.com>
2021-01-03 17:52:40 +0100

    create spinner component

---
+++ src/components/Spinner/Spinner.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Spinner.scss';
+
+const Spinner = () => {
+
+    return (
+        <div className="loader">
+            <img src="/img/loader-preview.svg" alt="loading" />
+        </div>
+    )
+}
+
+export default Spinner;
