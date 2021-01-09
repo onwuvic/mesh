f27c419cef56799d8aa8fca16ea15be8cc078ba1
Onwuzor Victor <onwuzorvictor@outlook.com>
2021-01-10 00:30:26 +0100

    add toast notification

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import './assets/scss/main.scss';
+import 'react-toastify/dist/ReactToastify.min.css';
 import PrivateRoute from './PrivateRoute';
 import UserProvider from './providers/UserProvider';
 import Home from './views/Home/Home';
 import LoginPage from './views/LoginPage/LoginPage';
 import SignupPage from './views/SignupPage/SignupPage';
+
+toast.configure();
 
 function App() {
   return (
