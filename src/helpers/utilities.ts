2ae902b6da698ea16e338c79e1581cc470314ac9
Onwuzor Victor <onwuzorvictor@outlook.com>
2021-01-17 22:01:58 +0100

    display date in a human friendly way

--- src/helpers/utilities.ts
+++ src/helpers/utilities.ts
@@ -1,13 +1,23 @@
+import moment from 'moment';
 import { IOrder } from "../models";
 
 export const formatOrderData = (orders: IOrder[]) => {
     return orders.map((item: any)=> {
         return {
           title: item.title,
-          bookingDate: typeof item.bookingDate !== "object" ? item.bookingDate : '--',
+          bookingDate: formatDate(item.bookingDate),
           address: (item.address && item.address.street) ? item.address.street : '--',
           customer: (item.customer && item.customer.name) ? item.customer.name : '--',
           id: item.id
         }
     });
+}
+
+const formatDate = (date: Date) => {
+  if (typeof date !== "object") {
+    return typeof date === 'number' 
+      ? moment.unix(date).format('DD-MM-YYYY')
+      : date;
+  }
+  return '--'
 }
